8d6c5b1e5f5a6bd8d3e7d7f0933991a56274d5f0
MeilCli <ics.sora.meil@gmail.com>
2021-02-23 20:52:36 +0900

    change: eslint script and refactor

--- src/main.ts
+++ src/main.ts
@@ -13,21 +13,15 @@ async function run() {
         execOption.listeners = {
             stdout: (data: Buffer) => {
                 stdout += data.toString();
-            }
+            },
         };
 
         await exec.exec(command, undefined, execOption);
 
-        if (
-            expectOutputRegex.length != 0 &&
-            stdout.match(expectOutputRegex) == null
-        ) {
+        if (expectOutputRegex.length != 0 && stdout.match(expectOutputRegex) == null) {
             core.setFailed(`stdout not match: ${expectOutputRegex}`);
         }
-        if (
-            expectOutputContains.length != 0 &&
-            stdout.includes(expectOutputContains) == false
-        ) {
+        if (expectOutputContains.length != 0 && stdout.includes(expectOutputContains) == false) {
             core.setFailed(`stdout not contain: ${expectOutputContains}`);
         }
     } catch (error) {
